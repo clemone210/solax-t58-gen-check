fd11ac17b34a4721efe5da27cc3070e4ef70e90e
Timo Clemens <43761260+clemone210@users.noreply.github.com>
2023-05-17 15:14:34 +0200

    revised code
    updated script and index

--- script.js
+++ script.js
@@ -1,51 +1,43 @@
-var results = [];
+let results = [];
+
+const machineCategories = {
+    "A": "T58 Master V1",
+    "B": "T58 Slave V1",
+    "E": "T58 Master V2",
+    "F": "T58 Slave V2",
+};
 
 function checkSerialNumber() {
-    var serialNumber = document.getElementById("serial-number").value.toUpperCase().replace(/ /g, "");
-    var machineCategory = serialNumber.substring(4, 5);
-    var description = "";
-    switch (machineCategory) {
-        case "A":
-            description = "T58 Master V1";
-            break;
-        case "B":
-            description = "T58 Slave V1";
-            break;
-        case "E":
-            description = "T58 Master V2";
-            break;
-        case "F":
-            description = "T58 Slave V2";
-            break;
-        default:
-            description = "Unknown machine category";
-    }
-    results.push({ serialNumber: serialNumber, category: machineCategory, description: description });
+    const serialNumber = document.getElementById("serial-number").value.toUpperCase().replace(/ /g, "");
+    const machineCategory = serialNumber.substring(4, 5);
+    const description = machineCategories[machineCategory] || "Unknown machine category";
+    
+    results.push({ serialNumber, category: machineCategory, description });
     displayResults();
 }
 
 function displayResults() {
-    var table = document.getElementById("results");
+    const table = document.getElementById("results");
     while (table.rows.length > 1) {
         table.deleteRow(1);
     }
-    for (var i = 0; i < results.length; i++) {
-        var tr = document.createElement("tr");
-        var tdSerialNumber = document.createElement("td");
-        tdSerialNumber.innerHTML = results[i].serialNumber;
-        tr.appendChild(tdSerialNumber);
-        var tdCategory = document.createElement("td");
-        tdCategory.innerHTML = results[i].category;
-        tr.appendChild(tdCategory);
-        var tdDescription = document.createElement("td");
-        tdDescription.innerHTML = results[i].description;
-        tr.appendChild(tdDescription);
+    for (let result of results) {
+        const { serialNumber, category, description } = result;
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${serialNumber}</td>
+            <td>${category}</td>
+            <td>${description}</td>
+        `;
         table.appendChild(tr);
     }
 }
+
 document.getElementById("serial-number").addEventListener("keydown", function(event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         checkSerialNumber();
     }
 });
+
+document.querySelector('button').addEventListener('click', checkSerialNumber);
